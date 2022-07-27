b8917afc7f92afa02d3164f0b12b3257387151d1
rizo928 <rizo928@users.noreply.github.com>
2022-07-27 12:35:45 -0400

    Added credential change ability

--- smb.py
+++ smb.py
@@ -27,6 +27,7 @@ Copywrite (C) 2022 Bitrox.io
 """
 
 # from pip imported libraries
+from distutils.log import info
 from flask import Flask, flash, redirect, url_for, render_template, request, session, abort
 from flask_session import Session  # flask-session
 from werkzeug.utils import secure_filename  # included w/ flask
@@ -40,6 +41,7 @@ from glob import glob
 import json
 import sys
 import os
+import logging
 import collections.abc
 
 app = Flask(__name__, static_url_path='',
@@ -47,68 +49,77 @@ app = Flask(__name__, static_url_path='',
             template_folder='templates')
 app.config['UPLOAD_FOLDER'] = "static/upload"
 
+# Unless you set the logger basic configuration, no log message is sent
+# with level INFO or lower regardless of what setLevel() is set to
+logging.basicConfig(level=logging.WARNING)
+app.logger.setLevel("INFO")
+
 # path to config file
 smb_config = os.path.join(os.path.expanduser("."), 'config.ini')
-config_parser = configparser.RawConfigParser()
-config_parser.read(smb_config)
+# config = configparser.RawConfigParser()
+config = configparser.ConfigParser()
+config.read(smb_config)
 
 disable_login = False
 ux_g = {}
-ux_g["app_name"] = "SM Access v0.9"
+ux_g["app_name"] = "SMB v0.02"
+user_id = "anonymous"
+user_pswd = ""
+user_pswd = None
 
 try:
-    app.config['ENV'] = 'production'
-    app.config["DEBUG"] = False  # config_parser.get('SMB', 'debug')
-    app.config["SECRET_KEY"] = config_parser.get('SMB', 'session_key')
-    user_id = config_parser.get('SMB', 'user_id')
-    user_pswd = config_parser.get('SMB', 'user_pswd')
-    if config_parser.get('SMB', 'disable_login') == 'True':
+    app.config['ENV'] = 'development'
+    app.config["DEBUG"] = True  # config.get('SMB', 'debug')
+    app.config["SECRET_KEY"] = config.get('SMB', 'session_key')
+    user_id = config.get('SMB', 'user_id')
+    user_pswd = config.get('SMB', 'user_pswd')
+    if config.get('SMB', 'disable_login') == 'True':
         disable_login = True
 
     # load any global variables to be passed to all calls to render_template
-    ux_g["album1_name"] = config_parser.get('SMB', 'album1_name')
-    ux_g["album1_id"] = config_parser.get('SMB', 'album1_id')
-    ux_g["album2_name"] = config_parser.get('SMB', 'album2_name')
-    ux_g["album2_id"] = config_parser.get('SMB', 'album2_id')
-    ux_g["album3_name"] = config_parser.get('SMB', 'album3_name')
-    ux_g["album3_id"] = config_parser.get('SMB', 'album3_id')
+    ux_g["album1_name"] = config.get('SMB', 'album1_name')
+    ux_g["album1_id"] = config.get('SMB', 'album1_id')
+    ux_g["album2_name"] = config.get('SMB', 'album2_name')
+    ux_g["album2_id"] = config.get('SMB', 'album2_id')
+    ux_g["album3_name"] = config.get('SMB', 'album3_name')
+    ux_g["album3_id"] = config.get('SMB', 'album3_id')
     ux_g["userid"] = "Anonymous"  # set at login
-    upload_path = config_parser.get('SMB', 'upload_path')
+    upload_path = config.get('SMB', 'upload_path')
     if upload_path == None:
         upload_path = "."
-    download_path = config_parser.get('SMB', 'download_path')
+    download_path = config.get('SMB', 'download_path')
     if download_path == None:
         download_path = "."
 
 except:
-    print('Missing config file, path: ', smb_config, file=sys.stderr)
+    app.logger.warning('Missing config file, path: ', smb_config)
     raise Exception("Config file is missing or corrupted.")
 
 # Check for needed subdirectories and try to create if they don't exist
 mode = 0o744
 if not os.path.isdir(upload_path):
-    print('Upload path does not exist, will try to creat it: ',
-          upload_path, file=sys.stderr)
+    app.logger.warning('Upload path does not exist, will try to creat it: %s',
+          upload_path)
     try:
         os.mkdir(upload_path, mode)
     except:
-        print('Exception creating upload path, exiting.', file=sys.stderr)
-        print('Check your config.ini for upload_path definition.', file=sys.stderr)
+        app.logger.error('Exception creating upload path, exiting.')
+        app.logger.error('Check your config.ini for upload_path definition.')
         quit()
     else:
-        print('Successfully created upload directory.', file=sys.stderr)
+        app.logger.info('Successfully created upload directory.')
 
 if not os.path.isdir(download_path):
-    print('Download path does not exist, will try to creat it: ',
-          download_path, file=sys.stderr)
+    app.logger.warning('Download path does not exist, will try to creat it: %s',
+          download_path)
     try:
         os.mkdir(download_path, mode)
     except:
-        print('Exception creating download path, exiting', file=sys.stderr)
-        print('Check your config.ini for download_path definition.', file=sys.stderr)
+        app.logger.error('Exception creating download path, exiting')
+        app.logger.error('Check your config.ini for download_path definition.')
         quit()
     else:
-        print('Successfully created download directory.', file=sys.stderr)
+        app.logger.info('Successfully created download directory.')
 
 # app.config["DEBUG"] = True
 app.config["SESSION_PERMANENT"] = False
@@ -116,7 +127,6 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)  # setup server side session from app
 
 sm = SmugMug()
-
 
 class dict2obj:
     # constructor
@@ -127,13 +137,12 @@ class dict2obj:
 # differently depending on whether or not we're dealing with iOS device
 def is_ios():
     uap = httpagentparser.detect(request.user_agent.string)
-    # print('User agent detect: ', uap, file=sys.stderr)
+    # print('User agent detect: ', uap)
     if uap['os']['name'] == 'iOS':
-        print('iPhone detected!', file=sys.stderr)
+        app.logger.info('iPhone detected!')
         return True
     else:
         return False
-
 
 def status_msg(the_msg):
     # allows returning either an array of status messages or a single status message
@@ -152,22 +161,34 @@ def test():
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if disable_login == True:
-        print('Login Disabled', file=sys.stderr)
+        app.logger.warning('Login Disabled')
         session["userid"] = 'Anonymous'
         return redirect("/")
     else:
-        print('Login Enabled', file=sys.stderr)
+        app.logger.info('Login Enabled')
+        if not session["userid"] == None:
+            # already logged in, just go to the home page
+            return redirect("/")
 
     if request.method == "POST":
-        # record the user name
-        if request.form.get("userid") != user_id:
-            print('Unknown user id', file=sys.stderr)
-            return render_template('login.html', statusmsg='Unknown credentials', ux_g=ux_g)
-        if request.form.get("pswd") != user_pswd:
-            print('Unknown user password', file=sys.stderr)
-            return render_template('login.html', statusmsg='Unknown credentials', ux_g=ux_g)
-        session["userid"] = request.form.get("userid")
-        ux_g["userid"] = session["userid"]
+        entered_id = request.form.get("userid")
+        entered_pwd = request.form.get("pswd")
+
+        if not entered_id.isidentifier():
+            app.logger.warning('Invalid login id entered')
+            return render_template('login.html', ux_g=ux_g)
+        if entered_id != user_id:
+            app.logger.warning('Unknown user id: '+entered_id+" : "+user_id)
+            return render_template('login.html', status_msg='Unknown credentials', ux_g=ux_g)
+
+        if not entered_pwd.isidentifier():
+            app.logger.warning('Invalid password entered')
+            return render_template('login.html', ux_g=ux_g)
+        if entered_pwd != user_pswd:
+            app.logger.warning('Unknown user password')
+            return render_template('login.html', status_msg='Unknown credentials', ux_g=ux_g)
+        session["userid"] = entered_id
+        ux_g["userid"] = entered_id
         # redirect to the main page
         return redirect("/")
     return render_template('login.html', ux_g=ux_g)
@@ -179,6 +200,49 @@ def logout():
     ux_g["userid"] = "Anonymous"
     return redirect("/login")
 
+@app.route("/credentials", methods=["POST", "GET"])
+def credentials():
+    global ux_g, user_id, user_pswd, config, smb_config
+    if not session.get("userid"):
+        ux_g["userid"] = "Anonymous"
+        return redirect("/login")
+
+    if request.method == "POST":
+        valid_credentials = True
+        if not request.form.get("loginID").isidentifier():
+            app.logger.warning('Invalid login id entered')
+            valid_credentials = False
+            # return render_template('login.html', statusmsg='Unknown credentials', ux_g=ux_g)
+        if not request.form.get("pswd").isidentifier():
+            app.logger.warning('Invalid password entered')
+            valid_credentials = False
+            # return render_template('login.html', statusmsg='Unknown credentials', ux_g=ux_g)
+        if not valid_credentials:
+            app.logger.warning('Invalid credentials entered')
+            return render_template('credentials.html', status_msg='Invalid credentials submitted', ux_g=ux_g)            
+
+        # session["userid"] = request.form.get("userid")
+        # ux_g["userid"] = session["userid"]
+        # redirect to the main page
+
+        try:
+
+            config['SMB']['user_id'] = request.form.get("loginID")
+            config['SMB']['user_pswd'] = request.form.get("pswd")
+            with open(smb_config, 'w') as configfile:    # save
+                config.write(configfile)
+            app.logger.info('Valid credentials entered and successfully updated')
+            user_id = request.form.get("loginID")
+            user_pswd = request.form.get("pswd")
+            session["userid"] = user_id
+            ux_g["userid"] = user_id
+
+            return status_msg('Login credentials successfully updated')
+        except:
+            app.logger.error('Error attempting to write credentials to config file.')
+            return status_msg('Error attempting to write credentials to config file, credentials unchanged!')
+    
+    return render_template('credentials.html', status_msg='', ux_g=ux_g)
 
 @app.route("/")
 def home():
@@ -208,7 +272,7 @@ def images():
     skip_images = request.args.get('noimages')
     album_id = request.args.get('album_id')
     if album_id == None:
-        print('Missing albumID', file=sys.stderr)
+        app.logger.warning('Missing albumID')
         return status_msg("Missing album_id parameter in /images")
 
     # setup paging variables (start = start page, stepsize = # of images/page, pages = # of pages)
@@ -250,14 +314,14 @@ def findimage():
     search_str = request.args.get('searchstr')
     skip_images = request.args.get('noimages')
     if album_id == None:
-        print('Missing albumID', file=sys.stderr)
+        app.logger.warning('Missing albumID')
         return status_msg("Missing albumID parameter for /findimage")
     if search_str == None:
-        print('Missing search_str', file=sys.stderr)
+        app.logger.warning('Missing search_str')
         return status_msg("Missing searchstr parameter for /findimage")
 
-    # print('Searching for image in album id: ', album_id, file=sys.stderr)
-    # print('Searching for: ', search_str, file=sys.stderr)
+    # print('Searching for image in album id: ', album_id)
+    # print('Searching for: ', search_str)
 
     images = sm.get_album_images(album_id)
     # sorted_images = sorted(images, key=lambda d: d['FileName'], reverse=False)
@@ -295,9 +359,9 @@ def image():
         return redirect("/login")
 
     image_id = request.args.get('image_id')
-    print('Received image id: ', image_id, file=sys.stderr)
+    app.logger.info('Received image id: %s', image_id)
     if image_id == None:
-        print('Missing image_id', file=sys.stderr)
+        app.logger.warning('Missing image_id')
         return status_msg("Missing image_id parameter for /image")
 
     image_core_info = sm.get_image_core_info(image_id)
@@ -305,7 +369,7 @@ def image():
     image_sizes = list(reversed(image_size_details['UsableSizes']))
         # We want the bigger sizes first in the list to avoid unnecessary scrolling
         # since modern blogs tend to use bigger image sizes
-    print('Image size key array: ', image_sizes, file=sys.stderr)
+    app.logger.info('Image size key array: %s', image_sizes)
     if image_core_info != None:
         image_info = {}
         image_info['ImageKey'] = image_core_info['ImageKey']
@@ -320,7 +384,7 @@ def image():
 
         return render_template('image.html', image_sizes=image_sizes, image_size_details=image_size_details, image_info=image_info, ux_g=ux_g)
     else:
-        print('Unsuccessful attempt to retrieve image core info', file=sys.stderr)
+        app.logger.error('Unsuccessful attempt to retrieve image core info')
         return status_msg("Unsuccessful attempt to retrieve image core info")
 
 
@@ -332,13 +396,12 @@ def pick():
     aidx = request.args.get('albumidx')
     if aidx == None:
         return status_msg("Missing album_num in /pick")
-    # print('album_num: ',aidx, file=sys.stderr)
+    # print('album_num: ',aidx)
     if int(aidx) < 1 or int(aidx) > 3:
         return status_msg("Valid album_num not passed to /pick: "+aidx)
     an_key = 'album'+str(aidx)+'_name'
     ai_key = 'album'+str(aidx)+'_id'
-    print('Ask user to take/pick an image to upload for ',
-          an_key+':'+ai_key, file=sys.stderr)
+    app.logger.info('Ask user to take/pick an image to upload for %s', an_key+':'+ai_key)
     return render_template('pick.html', ux_g=ux_g, album_name=ux_g[an_key], album_id=ux_g[ai_key]) # pick
 
 
@@ -352,25 +415,25 @@ def upload():
     if album_id == None:
         return status_msg("Missing album_id in /upload")
 
-    print('Album ID: ', album_id, file=sys.stderr)
+    app.logger.info('Album ID: %s', album_id)
 
     # get the list from the files object
     f_list = request.files.getlist("images")
-    print('Images: ', f_list, file=sys.stderr)
+    app.logger.info('Images: %s', f_list)
     smg = []
     counter = 0
     for f in f_list:
         counter += 1
         file_name = secure_filename(f.filename)
         file_name = os.path.join(upload_path, file_name)
-        print('Full local server upload path: ', file_name, file=sys.stderr)
+        app.logger.info('Full local server upload path: %s', file_name)
         # return status_msg("Check console for full upload path")
         try:
             f.save(file_name)
         except:
             return status_msg("Unable to upload image to local server.")
         else:
-            print('Successfully saved image locally on server', file=sys.stderr)
+            app.logger.info('Successfully saved image locally on server')
 
         image_data = sm.load_image(file_name)  # read it back in from the disk
         upload_response = sm.upload_image(
@@ -390,10 +453,9 @@ def download():
     image_info = {"ImageKey": "nQtF532",
                   "ArchivedMD5": "9682cc419980e7c3b72758d9b6bda989", "OriginalSize": "1529569"}
     image_path = "downloaded.jpg"
-    print('Downloading image...', file=sys.stderr)
+    app.logger.info('Downloading image...')
     response = sm.download_image(image_info, image_path, retries=2)
     return status_msg('Downloaded image from Test Blog') # download
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
